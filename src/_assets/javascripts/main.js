0800937c1c8d035fb863f9a7ad8bb4562794b66a
Tom Beck <tom@tastylounge.com>
2016-02-19 10:02:38 -0800

    added search form in topnav

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -5,3 +5,18 @@
 // $(function() {
 //     alert("HELLO");
 // });
+
+$(document).on('ready', function() {
+  $( "#search-form").on('click', function () {
+    $(this).addClass('active');
+    $submit = $(this).find('input[type="submit"]');
+  });
+  $('input.keyterms').on('keyup', function(){
+    window.console.log($(this).val());
+      if($(this).val().length !=0) {
+        $submit.attr('disabled', false);
+      } else {
+        $submit.attr('disabled',true);
+      }
+  });
+});
