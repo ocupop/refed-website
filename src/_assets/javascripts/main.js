9c094f8e76fc1165f2eaf8979cfa7d378a957451
Tom Beck <tom@tastylounge.com>
2016-02-23 23:01:56 -0800

    sorting animation

--- src/_assets/javascripts/main.js
+++ src/_assets/javascripts/main.js
@@ -23,18 +23,19 @@ $(document).on('ready', function() {
       }
   });
 
-  window.console.log(solution_data);
   solution_data.sort.forEach(function(sort) {
-    window.console.log(sort);
     $tab = $($.parseHTML("<li class='tab sort'></li>"));
-    $tab.attr('data-sort', sort.slug).text(sort.label).appendTo(".sort.tabs");
+    $tab.attr('data-sort', sort.slug).text(sort.label).appendTo(".tabs");
   })
 
-  $('.tab').on('click', function() {
+  $('.sort').on('click', function() {
     $(this).addClass('active').siblings().removeClass('active');
     var sort = $(this).attr('data-sort');
-
     showStat(sort);
+    $('#solution-cards').mixItUp('multiMix', {
+      sort: sort+':dsc'
+    });
     // chart.update(getData(filter));
   });
+  $("[data-sort='financial-benefit']").trigger('click');
 });
