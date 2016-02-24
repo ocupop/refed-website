40a9c2e7dae924898639898007ddd10d08be2041
Tom Beck <tom@tastylounge.com>
2016-02-23 21:35:30 -0800

    update to include hyphens

--- src/_assets/javascripts/_solutions.js
+++ src/_assets/javascripts/_solutions.js
@@ -1,4 +1,41 @@
-var solution_data = {% include solution_data.js %};
+{% assign solutions = site.solutions | sort:"order" %}
+var solution_data = {
+  sort: [
+    {
+      label: "Financial Benefit",
+      slug: "financial-benefit"
+    }, {
+      label: "Waste Diverted",
+      slug: "waste-diverted"
+    }, {
+      label: "Meals Saved",
+      slug: "meals-saved"
+    }, {
+      label: "Emissions Reduced",
+      slug: "emissions-reduced"
+    }, {
+      label: "Water Saved",
+      slug: "water-saved"
+    }, {
+      label: "Jobs Created",
+      slug: "jobs-created"
+    }
+  ],
+  solutions: [
+    {% for solution in solutions %}{
+      title: "{{ solution.title }}",
+      slug: "{{ solution.slug }}",
+      summary: "{{ solution.summary }}",
+      type: "{{ solution.type }}",
+      featured_image: "{{ solution.featured-image }}",
+      stakeholders: [{% for stakeholder in solution.stakeholders %}"{{ stakeholder }}"{% unless forloop.last %}, {% endunless %}{% endfor %}],
+      connected_solutions: [{% for s in solution.connected-solutions %}"{{ s }}"{% unless forloop.last %}, {% endunless %}{% endfor %}],
+      data: {
+        {% for i in solution.impact %}{{ i[0] | replace: '-', '_' }}: {{ i[1] }}{% unless forloop.last %}, {% endunless %}{% endfor %}
+      }
+    }{% unless forloop.last %}, {% endunless %}{% endfor %}
+  ]
+}
 
 
 function getFilters(data) {
