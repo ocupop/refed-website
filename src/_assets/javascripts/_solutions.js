bbbbf6b563aae96c3abdfaca2ab51acf0df3cab9
Tom Beck <tom@tastylounge.com>
2016-02-25 13:53:33 -0800

    updated icons and assets

--- src/_assets/javascripts/_solutions.js
+++ src/_assets/javascripts/_solutions.js
@@ -58,5 +58,7 @@ function getData(key) {
   }
 };
 function showStat(sort) {
+  var icon_class = "icon icon-"+sort;
+  $('.card header i').attr('class', icon_class);
   $('.card .stat').hide().filter("."+sort).show();
 };
