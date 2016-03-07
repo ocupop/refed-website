3be75efb6a382e2d573224e1a69dd3c8b63deca9
Tom Beck <tom@tastylounge.com>
2016-03-07 10:22:51 -0700

    client updates

--- src/_plugins/filters.rb
+++ src/_plugins/filters.rb
@@ -1,9 +1,14 @@
+require 'active_support/inflector'
 module Jekyll
   module Filters
     def add_commas(number)
       whole, decimal = number.to_s.split(".")
       whole_with_commas = whole.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
       [whole_with_commas, decimal].compact.join(".")
+    end
+
+    def singularize(str)
+      ActiveSupport::Inflector.singularize(str)
     end
   end
 end
