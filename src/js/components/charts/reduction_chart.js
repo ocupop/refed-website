a1db3d6d6ce9494798a64362fec8670e92f3263a
Tom Beck <tom@tastylounge.com>
2017-01-20 10:35:33 -0800

    cleanup: homepage

---
+++ src/js/components/charts/reduction_chart.js
@@ -0,0 +1,146 @@
+/* Copyright (C) 2017 ReFED
+ * 
+ * http://www.refed.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see
+ * http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+(function() {
+
+$.behaviors('.reduction_chart', initCharts);
+
+  function initCharts(container) {
+
+    var data = {
+        series: [20, 30, 50],
+        labels: ['', '']
+    };
+    var timeline = {
+        series: [30, 20],
+        labels: ['', '']
+    };
+
+    var timeline_options = {
+      donut: true,
+      donutWidth: 8,
+      startAngle: 0,
+      total: 100,
+      showLabel: false,
+      width: 200,
+      height: 200
+    }
+    var options = {
+      donut: true,
+      donutWidth: 70,
+      startAngle: 0,
+      total: 100,
+      showLabel: false,
+      plugins: [
+        Chartist.plugins.fillDonut({
+          items: [{
+            content: '<i class="fa fa-tachometer text-muted"></i>',
+            position: 'bottom',
+            offsetY : 10,
+            offsetX: -2
+          }, {
+            content: '<h3 class="series-a">20%</h3><h3 class="series-b">50%</h3>'
+          }]
+        })
+      ]
+    }
+
+    var innerChart = new Chartist.Pie('#reduction-timeline', timeline, timeline_options);
+    var chart = new Chartist.Pie('#reduction-goal', data, options);
+
+    innerChart.on('draw', function(data) {
+      if(data.type === 'slice') {
+        // Get the total path length in order to use for dash array animation
+        var pathLength = data.element._node.getTotalLength();
+
+        // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+        data.element.attr({
+          'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+        });
+
+        // Create animation definition while also assigning an ID to the animation for later sync usage
+        var animationDefinition = {
+          'stroke-dashoffset': {
+            id: 'anim' + data.index,
+            dur: 1000,
+            from: -pathLength + 'px',
+            to:  '0px',
+            easing: Chartist.Svg.Easing.easeOutQuint,
+            // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+            fill: 'freeze'
+          }
+        };
+
+        // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+        if(data.index !== 0) {
+          animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+        }
+
+        // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+        data.element.attr({
+          'stroke-dashoffset': -pathLength + 'px'
+        });
+
+        // We can't use guided mode as the animations need to rely on setting begin manually
+        // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+        data.element.animate(animationDefinition, false);
+      }
+    });
+    chart.on('draw', function(data) {
+      if(data.type === 'slice') {
+        // Get the total path length in order to use for dash array animation
+        var pathLength = data.element._node.getTotalLength();
+
+        // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+        data.element.attr({
+          'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+        });
+
+        // Create animation definition while also assigning an ID to the animation for later sync usage
+        var animationDefinition = {
+          'stroke-dashoffset': {
+            id: 'anim' + data.index,
+            dur: 1000,
+            from: -pathLength + 'px',
+            to:  '0px',
+            easing: Chartist.Svg.Easing.easeOutQuint,
+            // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+            fill: 'freeze'
+          }
+        };
+
+        // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+        if(data.index !== 0) {
+          animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+        }
+
+        // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+        data.element.attr({
+          'stroke-dashoffset': -pathLength + 'px'
+        });
+
+        // We can't use guided mode as the animations need to rely on setting begin manually
+        // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+        data.element.animate(animationDefinition, false);
+      }
+    });
+
+  }
+
+})();
