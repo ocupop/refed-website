b51259041673f8a2cf0b08912ed0fa25de38d15c
Tom Beck <tom@tastylounge.com>
2017-01-25 20:47:19 -0800

    added menuToggle behavior

--- src/js/main.js
+++ src/js/main.js
@@ -12,19 +12,10 @@ $(document).on('ready', function() {
     }, 1500);
   }
 
-  
-  // RESPONSIVE MENU
-  $('.menu-toggle').on('click', function(e) {
-    e.stopPropagation();
-    $("#page-header").toggleClass('open-menu');
-  });
-  // $("body").on('click', function() {
-  //   if ($("#page-header").hasClass('open-menu')) {
-  //     $("#page-header").removeClass("open-menu");
-  //   }
-  // });
+
 
 });
 
 
 
+  
