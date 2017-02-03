a4fffd0aac2176ad0d171c34d34a387296d82451
Tom Beck <tom@tastylounge.com>
2017-02-03 09:45:19 -0800

    updates external links and added a fallback for ensuring that they all open in a new window

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,19 @@ $(document).on('ready', function() {
       $('#thanks').fadeOut('slow');
     }, 1500);
   }
+
+  // Make all external links open in another window.
+  $('a').each(function() {
+     var a = new RegExp('/' + window.location.host + '/');
+     if(!a.test(this.href)) {
+       $(this).addClass('external');
+       $(this).click(function(event) {
+           event.preventDefault();
+           event.stopPropagation();
+           window.open(this.href, '_blank');
+       });
+     }
+  });
+
+
 });
