8aaf5c49ffe8ba658471042df44828bd24a1ffe1
Jon Burr <yojonathanwburr@gmail.com>
2017-02-07 12:03:43 -0800

    news templates, styles, js, sample posts for development

---
+++ src/js/components/articles.js
@@ -0,0 +1,18 @@
+$(function(){
+  var $container = $('.articles');
+  
+  $container.mixItUp();
+  
+  $('.sort-articles').on('click', function(){
+    var sort = $(this).data('sort');
+    window.console.log(sort);
+    if(sort == 'date:asc'){
+      $container.mixItUp('sort', sort);
+      $('.sort-articles').toggleClass('sort-dsc').data('sort', 'date:dsc');
+    }
+    if(sort == 'date:dsc'){
+      $container.mixItUp('sort', sort);
+      $('.sort-articles').toggleClass('sort-dsc').data('sort', 'date:asc');
+    }
+  });
+});
