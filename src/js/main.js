a35196e36fa1399622a7e6f66e6dbba46334de3f
Tom Beck <tom@tastylounge.com>
2017-02-20 09:06:30 -0800

    Updated external link logic so that it does not fire in editor mode.

--- src/js/main.js
+++ src/js/main.js
@@ -14,21 +14,23 @@ $(document).on('ready', function() {
 
   // TODO - Refactor into component
   // Make all external links open in another window.
-  $('a').each(function() {
-     var a = new RegExp('/' + window.location.host + '/');
-     var href = $(this).attr('href');
-     var mail = href.indexOf("mailto") >= 0 ? true: false;
+  var editing = $(body).hasClass('cms-editor-active');
+  if(!editing) {
+    $('a').each(function() {
+       var a = new RegExp('/' + window.location.host + '/');
+       var href = $(this).attr('href');
+       var mail = href.indexOf("mailto") >= 0 ? true: false;
 
-     // window.console.log("main.js:", mail, href);
-     if(!a.test(this.href) && !mail ) {
-       $(this).addClass('external');
-       $(this).click(function(event) {
-           event.preventDefault();
-           event.stopPropagation();
-           window.open(this.href, '_blank');
-       });
-     }
-  });
-
+       // window.console.log("main.js:", mail, href);
+       if(!a.test(this.href) && !mail ) {
+         $(this).addClass('external');
+         $(this).click(function(event) {
+             event.preventDefault();
+             event.stopPropagation();
+             window.open(this.href, '_blank');
+         });
+       }
+    });
+  }
 
 });
