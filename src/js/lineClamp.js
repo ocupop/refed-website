87eaaa2339510c96c6b6cee123615770d5ea774e
Jon Burr <yojonathanwburr@gmail.com>
2017-03-02 14:21:53 -0800

    fix for lineClamp.js bug, (still needs extensible solution, see comment in code for details)

--- src/js/lineClamp.js
+++ src/js/lineClamp.js
@@ -20,32 +20,57 @@
 (function() {
 
 $.behaviors('.lineClamp', clamp);
+$.behaviors('.unClamp', unclamp);
 
   function clamp(container) {
-
     container = $(container);
 
     var num_lines = container.data('numlines');
     var line_height = container.css('line-height').replace(/[^-\d\.]/g, '');  
     var clamp_height = num_lines * line_height;
+    window.console.log(clamp_height);
     var total_height = container.css('height').replace(/[^-\d\.]/g, '');
-    var unclamp = '<button class="unclamp">Read More</button>';
+    var unclamp = '<button class="unClamp">Read More</button>';
     if(clamp_height <= total_height){
       container.css('height', num_lines * line_height + 20);
-      $('<button class="unClamp">Read More</button>').insertAfter(container);
+      container.after(unclamp);
     }
+    
+  }
 
-    var unclamp = $('.unClamp');
+  function unclamp(container){
 
-    unclamp.on('click', function(){
-      if(container.hasClass('open')){
-        unclamp.text('Read more');
-        container.height(clamp_height).removeClass('open');
+    container = $(container);
+    container.on('click', function(){
+      // $(this).prev().toggleClass('open');
+      var clicked = $(this);
+      var target = clicked.prev();
+      window.console.log("lineclamp.js: target", target);
+      if(target.hasClass('open')){
+        clicked.text('Read more');
+        //Tom I need help to pass the clamp_height between these two functions
+        target.height(88.5713).removeClass('open');
       }else {
-        unclamp.text('Less');
-        container.height('auto').addClass('open');
+        clicked.text('Less');
+        target.height('auto').addClass('open');
       }
     });
   }
 
+  // function unclamp(container){
+  //   $('.unClamp').on('click', function(){
+
+  //   if(container.hasClass('open')){
+  //     $(this).text('Read more');
+  //     container.height(clamp_height).removeClass('open');
+  //   }else {
+  //     $(this).text('Less');
+  //     container.height('auto').addClass('open');
+  //   }
+  // });
+  // }
+  
+
+
 })();
+
