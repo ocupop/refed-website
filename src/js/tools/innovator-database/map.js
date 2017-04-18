a0ad89b6c1e949b7f46325c76adc83f57d9f04c7
Tom Beck <tom@tastylounge.com>
2017-04-18 05:23:07 -0700

    updated search location analytics messages

--- src/js/tools/innovator-database/map.js
+++ src/js/tools/innovator-database/map.js
@@ -238,7 +238,7 @@ $.behaviors('.innovatorDatabase_map', innovatorDatabase_map);
       innovatorMap.setZoom(8);
       innovatorMap.setCenter(searchLocation);
 
-      ga('send', 'event', 'searchLocation', 'innovator-database', location);
+      ga('send', 'event', 'search location', 'innovator database', location);
   });
 
 })();
