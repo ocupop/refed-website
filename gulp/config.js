922c5df3954cf07c3bb55d01610391f88dd22cd1
Tom Beck <tom@tastylounge.com>
2017-04-27 07:39:20 -0700

    use jekyll includes for bundling js

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,8 @@
-var clientname        = 'amara';
+var clientname        = 'refed';
 var src               = 'src';
 var srcAssets         = 'src/_assets';
 var bowerAssets       = 'bower_components';
+
 
 
 module.exports = {
@@ -14,5 +15,28 @@ module.exports = {
       bowerrc: './.bowerrc',
       bowerJson: './bower.json'
     }
-  }
+  },
+  optimize: {
+    css: {
+      src:  srcAssets + '/css/*.css',
+      dest: '/css/',
+      options: {
+        keepSpecialComments: 0
+      }
+    },
+    js: {
+      src:  srcAssets + '/js/*.js',
+      dest: '/js/',
+      options: {}
+    },
+    images: {
+      src:  srcAssets + '/img/**/*.{jpg,jpeg,png,gif}',
+      dest: '/img/',
+      options: {
+        optimizationLevel: 3,
+        progessive: true,
+        interlaced: true
+      }
+    }
+  },
 };
