15acde3afb65ba5b3cb8452682cec3f67c67ee14
Tom Beck <tom@ocupop.com>
2018-01-18 13:26:17 -0700

    updates and fixes

---
+++ src/_includes/_assets/js/nav/pagenav.js
@@ -0,0 +1,33 @@
+/* Copyright (C) 2017 Ocupop
+ * 
+ * http://www.ocupop.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see
+ * http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+(function() {
+
+$.behaviors('.pagenav a', pagenav);
+
+  function pagenav(container) {
+    container = $(container);
+
+    container.on('click', function(e){
+      e.preventDefault();
+      var target = $(this).attr('href');
+      pageScroll(target, 50);
+    });
+  }
+
+})();
