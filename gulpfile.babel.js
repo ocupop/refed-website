0aeb710cc7ad9144907dbeeaace0b58199f8c76e
Jon Burr <jonathanwburr@gmail.com>
2020-09-15 13:53:49 -0700

    refactor with new build, remove gatsby

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
-const gulp = require("gulp");
-const suite = require('@cloudcannon/suite');
+const gulp = require("gulp")
+const suite = require('@cloudcannon/suite')
+const { scripts } = require('./tasks/scripts')
 
-suite.dev(gulp, {
-  "jekyll": {
-    "src": "content"
-  }
-});
+suite.dev(gulp)
+
+gulp.task('default', gulp.parallel('scripts', 'dev'));
+
+exports.scripts = scripts
+
 
