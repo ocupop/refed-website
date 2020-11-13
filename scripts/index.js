6a4708f7f603a1f1f0b203fc6d3d98dba79d6b40
tombeck <t@ocupop.com>
2020-11-13 13:57:05 -0800

    clean up settings

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import HelloWorld from './HelloWorld'
-import StakeholdersList from './StakeholdersList'
-import SolutionDetail from './SolutionDetail'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import HelloWorld from './HelloWorld'
+// import StakeholdersList from './StakeholdersList'
+// import SolutionDetail from './SolutionDetail'
 
-const COMPONENTS = {
-  HelloWorld,
-  StakeholdersList,
-  SolutionDetail
-}
+// const COMPONENTS = {
+//   HelloWorld,
+//   StakeholdersList,
+//   SolutionDetail
+// }
 
-function renderComponentInElement(el) {
-  var Component = COMPONENTS[el.dataset.component];
-  if (!Component) return;
-  // get props from elements data attribute, like the post_id
-  const props = Object.assign({}, el.dataset);
-  ReactDOM.render(<Component {...props} />, el);
-}
+// function renderComponentInElement(el) {
+//   var Component = COMPONENTS[el.dataset.component];
+//   if (!Component) return;
+//   // get props from elements data attribute, like the post_id
+//   const props = Object.assign({}, el.dataset);
+//   ReactDOM.render(<Component {...props} />, el);
+// }
 
-document
-  .querySelectorAll('.__react-component')
-  .forEach(renderComponentInElement)
+// document
+//   .querySelectorAll('.__react-component')
+//   .forEach(renderComponentInElement)
 
 // Navbar collapse sub-menu
 var matches = document.querySelectorAll('.nav-back-button');
-if(matches){
-  matches.forEach(function(btn) {
+if (matches) {
+  matches.forEach(function (btn) {
     btn.addEventListener("click", (e) => {
       e.preventDefault();
       var closestparent = btn.closest('.dropdown-menu');
@@ -40,18 +40,19 @@ var w = window.innerWidth;
 var controller;
 var size = w > 768 ? "big" : "small";
 //if (size === "big") {
-  makeScrollMagic();
+makeScrollMagic();
 //}
 
 function makeScrollMagic() {
   controller = new ScrollMagic.Controller();
   var scene = new ScrollMagic.Scene({
-    triggerElement: ".sticky-nav",
+    triggerElement: ".subnav",
     triggerHook: 0, // 0=top, 0.5=middle, 1=bottom
   })
-    .setClassToggle(".sticky-nav", "minimized") // add class toggle
-    .setClassToggle(".scrollmagic-pin-spacer", "minimized") // add class toggle
-    .setPin(".sticky-nav")
+    .setClassToggle(".subnav", "fixed-top") // add class toggle
+    .setPin(".subnav", {
+      // spacerClass: "stickynav"
+    })
     //.addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
     .addTo(controller);
 }
@@ -93,8 +94,8 @@ window.addEventListener("resize", sizeIt);
 /* CAROUSELS */
 /* HOMEPAGE - NEWS CAROUSEL UNDER THE HERO */
 var elem = document.querySelector('.news-carousel');
-if(elem){
-  var flkty_featurednews = new Flickity( elem, {
+if (elem) {
+  var flkty_featurednews = new Flickity(elem, {
     prevNextButtons: true,
     pageDots: false,
     "wrapAround": true,
@@ -133,80 +134,80 @@ if(elem){
 // }
 
 /* This adds a class of "flickity-resize" to carousels after they have initialized */
-Flickity.prototype._createResizeClass = function() {
-var that = this;
-setTimeout(function(){ 
-  that.element.classList.add('flickity-resize');
+Flickity.prototype._createResizeClass = function () {
+  var that = this;
+  setTimeout(function () {
+    that.element.classList.add('flickity-resize');
   }, 1000);
 };
 
 Flickity.createMethods.push('_createResizeClass');
 
 var resize = Flickity.prototype.resize;
-Flickity.prototype.resize = function() {
+Flickity.prototype.resize = function () {
   this.element.classList.remove('flickity-resize');
-  resize.call( this );
+  resize.call(this);
   this.element.classList.add('flickity-resize');
 };
 
 /* Carousels - no dots by default */
 var carousel = document.querySelectorAll('.carousel-refed');
-if(carousel){
-  carousel.forEach(function(carousel){
-    var flkty_logos = new Flickity( carousel, {
-    contain: true,
-    cellAlign: 'left',
-    "wrapAround": true,
-    prevNextButtons: true,
-    arrowShape: { x0: 15,x1: 80, y1: 50,x2: 80, y2: 50,x3: 80},
-    pageDots: false,
-    adaptiveHeight: false,
-    on: {
-      ready: function() {
-        this.element.classList.add('flickity-init')
+if (carousel) {
+  carousel.forEach(function (carousel) {
+    var flkty_logos = new Flickity(carousel, {
+      contain: true,
+      cellAlign: 'left',
+      "wrapAround": true,
+      prevNextButtons: true,
+      arrowShape: { x0: 15, x1: 80, y1: 50, x2: 80, y2: 50, x3: 80 },
+      pageDots: false,
+      adaptiveHeight: false,
+      on: {
+        ready: function () {
+          this.element.classList.add('flickity-init')
+        }
       }
-    }
     })
   });
 }
 
 /* Carousels - with dots */
 var carousel_with_dots = document.querySelectorAll('.carousel-refed-dots');
-if(carousel_with_dots){
-  carousel_with_dots.forEach(function(carousel_with_dots){
-    var flkty_logos = new Flickity( carousel_with_dots, {
-    contain: true,
-    cellAlign: 'left',
-    "wrapAround": true,
-    prevNextButtons: true,
-    arrowShape: { x0: 15,x1: 80, y1: 50,x2: 80, y2: 50,x3: 80},
-    pageDots: true,
-    adaptiveHeight: false,
-    on: {
-      ready: function() {
-        this.element.classList.add('flickity-init')
+if (carousel_with_dots) {
+  carousel_with_dots.forEach(function (carousel_with_dots) {
+    var flkty_logos = new Flickity(carousel_with_dots, {
+      contain: true,
+      cellAlign: 'left',
+      "wrapAround": true,
+      prevNextButtons: true,
+      arrowShape: { x0: 15, x1: 80, y1: 50, x2: 80, y2: 50, x3: 80 },
+      pageDots: true,
+      adaptiveHeight: false,
+      on: {
+        ready: function () {
+          this.element.classList.add('flickity-init')
+        }
       }
-    }
     })
   });
 }
 
 /* Carousels - with dots */
 var carousel_dots_only = document.querySelectorAll('.carousel-refed-dots-only');
-if(carousel_dots_only){
-  carousel_dots_only.forEach(function(carousel_dots_only){
+if (carousel_dots_only) {
+  carousel_dots_only.forEach(function (carousel_dots_only) {
     var refed_dots_only = new Flickity(carousel_dots_only, {
-    contain: true,
-    cellAlign: 'left',
-    "wrapAround": true,
-    prevNextButtons: false,
-    pageDots: true,
-    adaptiveHeight: false,
-    on: {
-      ready: function() {
-        this.element.classList.add('flickity-init')
+      contain: true,
+      cellAlign: 'left',
+      "wrapAround": true,
+      prevNextButtons: false,
+      pageDots: true,
+      adaptiveHeight: false,
+      on: {
+        ready: function () {
+          this.element.classList.add('flickity-init')
+        }
       }
-    }
     })
   });
 }
@@ -242,82 +243,82 @@ contain: true,
 
 /* BEGIN filterable list */
 var filtered_list = document.querySelectorAll('.list-with-filter'); // for each dropdown on the page
-if(filtered_list){
-  filtered_list.forEach(function(section) {
-  init_dropdown(section);
-  init_filter_buttons(section);
+if (filtered_list) {
+  filtered_list.forEach(function (section) {
+    init_dropdown(section);
+    init_filter_buttons(section);
   });
 }
 
-function init_dropdown(section){
-var dropdown = section.querySelector('.dropdown');
-var options = dropdown.querySelectorAll('.dropdown-item');
-if(options){
-  options.forEach(function(btn) { // for each option in this dropdown
-    dropdown.querySelector('.dropdown-select').textContent = options[0].textContent;
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
+function init_dropdown(section) {
+  var dropdown = section.querySelector('.dropdown');
+  var options = dropdown.querySelectorAll('.dropdown-item');
+  if (options) {
+    options.forEach(function (btn) { // for each option in this dropdown
+      dropdown.querySelector('.dropdown-select').textContent = options[0].textContent;
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
 
-      var value = btn.getAttribute("data-value");
-      var list = section.querySelector('.filterable-list');
+        var value = btn.getAttribute("data-value");
+        var list = section.querySelector('.filterable-list');
 
-      filterList(section,value);
-    }); 
-  });
+        filterList(section, value);
+      });
+    });
   }
 }
 
 
-function init_filter_buttons(section){
-/* add event listeners to the CASE STUDIES category pill (on the "our Impact" page) */
-var pills = section.querySelectorAll('.case-study-filter');
-if(pills){
-pills.forEach(function(pill) { // for each option in this dropdown
-  pill.style.cursor = 'pointer';
-  pill.addEventListener("click", (e) => {
-    e.preventDefault();
-    var filter = pill.getAttribute("data-filter");
-    var elem = pill.closest('.horizontal-carousel-arrows-only');
-    filterList(section,filter);
+function init_filter_buttons(section) {
+  /* add event listeners to the CASE STUDIES category pill (on the "our Impact" page) */
+  var pills = section.querySelectorAll('.case-study-filter');
+  if (pills) {
+    pills.forEach(function (pill) { // for each option in this dropdown
+      pill.style.cursor = 'pointer';
+      pill.addEventListener("click", (e) => {
+        e.preventDefault();
+        var filter = pill.getAttribute("data-filter");
+        var elem = pill.closest('.horizontal-carousel-arrows-only');
+        filterList(section, filter);
 
-  }); 
-}); 
+      });
+    });
+  }
 }
-}
 
 
 
 
 
-function filterList(section,filter){
-  console.log("FILTER = "+filter)
+function filterList(section, filter) {
+  console.log("FILTER = " + filter)
   var dropdown = section.querySelector('.dropdown');
   var list = section.querySelector('.filterable-list');
 
   // SHOW/HIDE lements based on which filter is selected.
   var listitems = list.querySelectorAll('.filterable-list-item');
-  for (var i=0; i < listitems.length; i++) {
+  for (var i = 0; i < listitems.length; i++) {
     listitems[i].style.display = '';
   }
-  if(filter !== 'all'){
-    var listitems = list.querySelectorAll('.card:not(.'+filter+')');  
-    for (var i=0; i < listitems.length; i++) {
-        listitems[i].style.display = 'none';
+  if (filter !== 'all') {
+    var listitems = list.querySelectorAll('.card:not(.' + filter + ')');
+    for (var i = 0; i < listitems.length; i++) {
+      listitems[i].style.display = 'none';
     }
   }
 
 
-  // change the value of the dropdown menu as well.  
+  // change the value of the dropdown menu as well.
   var options = dropdown.querySelectorAll('.dropdown-item');
-  for (var i=0; i < options.length; i++) {
+  for (var i = 0; i < options.length; i++) {
     var label = 'Select';
-    if(options[i].getAttribute('data-value') == filter){
+    if (options[i].getAttribute('data-value') == filter) {
       var label = options[i].textContent;
       break;
     }
-  }  
+  }
   dropdown.querySelector('.dropdown-select').textContent = label;
-  var flkty = Flickity.data( list )
+  var flkty = Flickity.data(list)
   flkty.resize(); // TODO: this should be variable.
 }
 
@@ -325,19 +326,19 @@ function filterList(section,filter){
 
 
 var btns = document.querySelectorAll('.toggle-newsletter-modal');
-if(btns){
-btns.forEach(function(btn) { // for each option in this dropdown
-  btn.style.cursor = 'pointer';
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    var body = document.getElementsByTagName("BODY")[0]
-    body.classList.toggle("show-subscribe-form");
-  }); 
-}); 
+if (btns) {
+  btns.forEach(function (btn) { // for each option in this dropdown
+    btn.style.cursor = 'pointer';
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      var body = document.getElementsByTagName("BODY")[0]
+      body.classList.toggle("show-subscribe-form");
+    });
+  });
 }
 
 //generate ics file to download
-$('.ics-download').on('click', function(){
+$('.ics-download').on('click', function () {
   var cal_single = ics();
   var subject = $(this).data('subject');
   var description = $(this).data('description');
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -3,10 +3,10 @@ import webpack from 'webpack-stream'
 import { webpackConfig } from './webpack'
 
 gulp.task('scripts', function () {
-  return gulp.src('./scripts/index.js')
+  return gulp.src('./scripts/**/*')
     .pipe(webpack(webpackConfig, null, function (err, stats) {
       /* Use stats to do more things if needed */
-      // console.log(stats)
+      console.log(err)
     }))
     .pipe(gulp.dest('./src'));
 })
